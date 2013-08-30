4c9eda526bfa74024c8860da558b088e7a77fb88
Justin Threlkeld <justin@nashvilleparent.com>
2013-08-30 14:59:28 -0500

    started script action outline.

---
+++ src/socialNative.js
@@ -0,0 +1,21 @@
+/* script diagram:
+Check: are we on mobile?
+
+if we are on mobile
+  find all `ul.social a`s and iterate over them
+    if `href` begins with any of {{social network urls}} / alternatively: / if class is any of {{app names}}
+      on click
+        add a new action on click: goto {{app name}:{uri scheme handler}}
+
+
+URI Schema Handlers: (waiting until called)
+
+app name: twitter
+http link structure: http://twitter.com/{{username}}
+uri scheme: twitter://user?screen_name={{username}}
+
+app name: facebook
+http link structure: http://facebook.com/{{username}|{user id}|{page name}|{page id} }
+uri scheme: ***unknown/unavailable/broken?***
+
+*/
